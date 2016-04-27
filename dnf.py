5990ee8397c36a5b8734ad9f561540dea21293e7
Jeremy Fix <jeremy.fix@gmail.com>
2016-04-27 13:48:39 +0200

    Move the optimized convolution in tools

--- dnf.py
+++ dnf.py
@@ -113,84 +113,13 @@ class DNF:
             # If weights_name == 'optim_step', we do not have to build anything
             pass
 
+    @profile
     def get_lateral_contributions(self):
         if self.weights_name in ["dog", "doe", "dol", "step"]:
             # We use the generic computation with a FFT based convolution
             return tools.cconv(self.fu, self.w) 
         elif self.weights_name == 'optim_step':
-            return self.lateral_optim_step()
-
-    def lateral_optim_step(self):
-        # Reminder : the Step function is here defined as :
-        #a stepwise : w(x) = Ae 1_(|x| < ke si) - ki Ae 1_(|x| < si)
-        # 1 params : Ae >= 0 ; ke in [0, 1], ki in [0,1] ,  si > 0
-        
-        Ae, ke, ki, si = self.w_params
-        se = ke * si
-        Ai = ki * Ae
-
-        lat = np.zeros(self.size)
-        # We need to compute the lateral contribution at the first position 
-        # with an order of N sums and 2 products            
-        N = self.size[0]
-        # We identify the corner points of the steps
-        # which are the ones for which the lateral contribution will change as we move  
-        # along a sliding window
-        #          -------
-        #          |     |
-        # ---------=     =---------
-        # On the above step function, the corner points are denoted by "="
-        # In corners[0] is the leftmost, corners[1] is the rightmost
-        # Mind that we use a toric topology, so that for the first unit 
-        # the leftmost corner is actually on the right because of the wrap around
-
-        # we must handle the specific cases where the excitatory or inhibitory
-        # radii cover the whole field
-        
-        all_excitatories = se >= N-1
-        if not all_excitatories:
-            corners_exc = [int(np.ceil(N-se)), int(np.floor(se))]
-            lat_e = Ae * (sum(itertools.islice(self.fu, 0, corners_exc[1]+1)) + sum(itertools.islice(self.fu,corners_exc[0],None)))
-        else:
-            lat_e = Ae * sum(self.fu)
-
-        all_inhibitories = si >= N-1
-        if not all_inhibitories:
-            corners_inh = [int(np.ceil(N-si)), int(np.floor(si))]
-            lat_i = Ai * (sum(itertools.islice(self.fu,0, corners_inh[1]+1)) + sum(itertools.islice(self.fu,corners_inh[0], None)))
-        else:
-            lat_i = Ai * sum(self.fu)
-
-        lat[0] = lat_e - lat_i
-
-        for i in xrange(1, N):
-            # We can then compute the lateral contributions of the other locations
-            # with a sliding window by just updating the contributions
-            # at the "corners" of the weight function
-            if not all_excitatories:
-                lat_e -= Ae * self.fu[corners_exc[0]]
-                corners_exc[0] += 1
-                if(corners_exc[0] >= N):
-                    corners_exc[0] = 0
-            
-                corners_exc[1] += 1
-                if(corners_exc[1] >= N):
-                    corners_exc[1] = 0
-                lat_e += Ae * self.fu[corners_exc[1]]
-
-            if not all_inhibitories:
-                lat_i -= Ai * self.fu[corners_inh[0]]
-                corners_inh[0] += 1
-                if(corners_inh[0] >= N):
-                    corners_inh[0] = 0
-            
-                corners_inh[1] += 1
-                if(corners_inh[1] >= N):
-                    corners_inh[1] = 0
-                lat_i += Ai * self.fu[corners_inh[1]]
-
-            lat[i] = lat_e - lat_i
-        return lat
+            return tools.cconv_step(self.fu, self.w_params)
 
     def step(self, I):
         self.u = self.u + self.dt_tau * (-self.u + self.get_lateral_contributions()  + I + self.h)
@@ -216,9 +145,9 @@ if(__name__ == '__main__'):
         field.step(I)
     output_optim_step = field.get_output()
 
-    import matplotlib.pyplot as plt
+    #import matplotlib.pyplot as plt
     
-    f, axarr = plt.subplots(2, sharex=True)
-    axarr[0].plot(output_step,'b')
-    axarr[1].plot(output_optim_step,'r')
-    plt.show()
+    #f, axarr = plt.subplots(2, sharex=True)
+    #axarr[0].plot(output_step,'b')
+    #axarr[1].plot(output_optim_step,'r')
+    #plt.show()
--- tools.py
+++ tools.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-
+import itertools
 
 def cconv(a, b):
     '''
@@ -19,6 +19,80 @@ def cconv2(a, b):
     Computes the circular convolution of the (real-valued) matrices a and b.
     '''
     return np.fft.ifft2(np.fft.fft2(a) * np.fft.fft2(b)).real
+
+def cconv_step(fu, w_params):
+    # Reminder : the Step function is here defined as :
+    #a stepwise : w(x) = Ae 1_(|x| < ke si) - ki Ae 1_(|x| < si)
+    # 1 params : Ae >= 0 ; ke in [0, 1], ki in [0,1] ,  si > 0
+    
+    Ae, ke, ki, si = w_params
+    se = ke * si
+    Ai = ki * Ae
+
+    N = fu.shape[0]
+
+    lat = np.zeros((N,))
+    # We need to compute the lateral contribution at the first position 
+    # with an order of N sums and 2 products            
+    # We identify the corner points of the steps
+    # which are the ones for which the lateral contribution will change as we move  
+    # along a sliding window
+    #          -------
+    #          |     |
+    # ---------=     =---------
+    # On the above step function, the corner points are denoted by "="
+    # In corners[0] is the leftmost, corners[1] is the rightmost
+    # Mind that we use a toric topology, so that for the first unit 
+    # the leftmost corner is actually on the right because of the wrap around
+
+    # we must handle the specific cases where the excitatory or inhibitory
+    # radii cover the whole field
+    
+    all_excitatories = se >= N-1
+    if not all_excitatories:
+        corners_exc = [int(np.ceil(N-se)), int(np.floor(se))]
+        lat_e = Ae * (sum(itertools.islice(fu, 0, corners_exc[1]+1)) + sum(itertools.islice(fu,corners_exc[0],None)))
+    else:
+        lat_e = Ae * sum(fu)
+
+    all_inhibitories = si >= N-1
+    if not all_inhibitories:
+        corners_inh = [int(np.ceil(N-si)), int(np.floor(si))]
+        lat_i = Ai * (sum(itertools.islice(fu,0, corners_inh[1]+1)) + sum(itertools.islice(fu,corners_inh[0], None)))
+    else:
+        lat_i = Ai * sum(fu)
+
+    lat[0] = lat_e - lat_i
+
+    for i in xrange(1, N):
+        # We can then compute the lateral contributions of the other locations
+        # with a sliding window by just updating the contributions
+        # at the "corners" of the weight function
+        if not all_excitatories:
+            lat_e -= Ae * fu[corners_exc[0]]
+            corners_exc[0] += 1
+            if(corners_exc[0] >= N):
+                corners_exc[0] = 0
+        
+            corners_exc[1] += 1
+            if(corners_exc[1] >= N):
+                corners_exc[1] = 0
+            lat_e += Ae * fu[corners_exc[1]]
+
+        if not all_inhibitories:
+            lat_i -= Ai * fu[corners_inh[0]]
+            corners_inh[0] += 1
+            if(corners_inh[0] >= N):
+                corners_inh[0] = 0
+        
+            corners_inh[1] += 1
+            if(corners_inh[1] >= N):
+                corners_inh[1] = 0
+            lat_i += Ai * fu[corners_inh[1]]
+
+        lat[i] = lat_e - lat_i
+    return lat
+
 
 def gaussian(d2, sigma, A):
     '''
