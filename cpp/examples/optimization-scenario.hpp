7570b1b3038946febcb115b227f541d283c49430
Jeremy Fix <jeremy.fix@gmail.com>
2017-01-31 06:32:10 +0100

    upd

--- cpp/examples/optimization-scenario.hpp
+++ cpp/examples/optimization-scenario.hpp
@@ -7,6 +7,7 @@ protected:
   unsigned int _nb_steps;
   std::vector<int> _shape;
   int _size;
+  Input input;
 public:
   Scenario(unsigned int nb_steps,
 	   std::vector<int> shape) :
@@ -15,6 +16,7 @@ public:
     _size = 1;
     for(auto s: shape)
       _size *= s;
+    input.resize(_size);
   }
 
   virtual double evaluate(std::shared_ptr<neuralfield::Network> net) = 0;
@@ -25,32 +27,80 @@ enum class CompetitionType {Random, Structured};
 
 template<CompetitionType T>
 class CompetitionScenario : public Scenario {
+
+private:
+  void generate_input();
 public:
 
   CompetitionScenario(unsigned int nb_steps,
 		      std::vector<int> shape) :
     Scenario(nb_steps, shape) {
   }
-  
-  double evaluate(std::shared_ptr<neuralfield::Network> net) override ;
 
-};
-
-template<>
-double CompetitionScenario<CompetitionType::Random>::evaluate(std::shared_ptr<neuralfield::Network> net) {
-  return 0.;
-}
-
-template<>
-double CompetitionScenario<CompetitionType::Structured>::evaluate(std::shared_ptr<neuralfield::Network> net) {
-  return 0.;
-}
-/*
-template<>
-class CompetitionScenario<CompetitionType::Structured> {
-  double evaluate(std::shared_ptr<neuralfield::Network> net) {
-    return 0.;
+  std::vector<double> local_argmax(void) {
+    if(_shape.size() == 1) {
+      return {0.};
+    }
+    else if(_shape.size() == 2) {
+      return {0., 0.};
+    }
+    else {
+      throw std::runtime_error("Cannot compute local argmax in dimensions higher than 2");
+    }
   }
+
+  std::vector<double> get_lower_bound(std::vector<double> max_pos) {
+  }
+
+  std::vector<double> get_upper_bound(std::vector<double> max_pos) {
+  }
+  
+  double evaluate(std::shared_ptr<neuralfield::Network> net) override {
+    // For a competition scenario, there must be a single bump
+    // its location depends on the argmax of a convoluted input
+    generate_input();
+    
+    net->reset();
+    
+    net->set_input<Input>("input", input);
+
+    for(unsigned int i = 0 ; i < _nb_steps; ++i)
+      net->step();
+    auto fu = net->get("fu");
+    
+    // We now evaluate the fitness
+    auto max_pos = local_argmax();
+
+    // Then we build up the templates
+    auto lb = get_lower_bound(max_pos);
+    auto ub = get_upper_bound(max_pos);
+
+    
+    std::vector<double>::iterator it_lb, it_ub;
+    it_lb = lb.begin();
+    it_ub = ub.begin();
+    double f = 0.0;
+    for(auto& v: *fu) {
+      if(v < *it_lb)
+	f += (v-*it_lb)*(v-*it_lb);
+      else if(v > *it_ub)
+	f += (v-*it_ub)*(v-*it_ub);
+      ++it_lb;
+      ++it_ub;
+    }
+    return f;
+  }
+
 };
-*/
+
+template<>
+void CompetitionScenario<CompetitionType::Random>::generate_input() {
+
+}
+
+template<>
+void CompetitionScenario<CompetitionType::Structured>::generate_input() {
+
+}
+
 
